006bd92a0638677aa489e74b75e6befbcce92c9b
endreman0 <endreman@squidgey.com>
2015-02-19 10:21:49 -0800

    Initial commit II

---
+++ app/src/androidTest/java/org/usfirst/frc/team4131/scoutingapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4131.scoutingapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

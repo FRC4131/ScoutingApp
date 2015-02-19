b3f30e277cac2c6ba1b9afcf581b07ccea96bae2
endreman0 <endreman@squidgey.com>
2015-02-19 12:29:51 -0800

    Button on 'Survey Submitted' page to return home
    There was an NPE finding the 'result' text. I don't know why, but I just cut it out.

--- app/src/main/java/org/usfirst/frc/team4131/scoutingapp/SubmitActivity.java
+++ app/src/main/java/org/usfirst/frc/team4131/scoutingapp/SubmitActivity.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4131.scoutingapp;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,15 +14,19 @@ public class SubmitActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        int number = Integer.valueOf(intent.getStringExtra(MainActivity.TEAM_NUMBER));
+        int teamNumber = 0;
+        try{
+            teamNumber = Integer.valueOf(intent.getStringExtra(MainActivity.TEAM_NUMBER));
+        }catch(NumberFormatException ex){
+            Log.e("SURVEY", "Invalid team number", ex);
+        }
         String teamName = intent.getStringExtra(MainActivity.TEAM_NAME);
         String robotName = intent.getStringExtra(MainActivity.ROBOT_NAME);
-        String text = "Team " + number + " (" + teamName + ")";
+        String text = "Team " + teamNumber + " (" + teamName + ")";
         if(robotName!=null && robotName.length()>0) text = text.concat(": " + robotName);
-        TextView view = new TextView(this);
-        view.setTextSize(30);
-        view.setText(text);
-        setContentView(view);
+        Log.w("Collected survey data", text);
+        //Eventually, data will be stored and used.
+        setContentView(R.layout.activity_submit);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -29,13 +34,15 @@ public class SubmitActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
-    public void goHome(View view){
+    public void restart(View view){
         startActivity(new Intent(this, MainActivity.class));
     }
 }
